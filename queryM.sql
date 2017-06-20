f640d65cf72449bc2e07e1062bb6b2453519b840
Wellington Felix <wfmf@cin.ufpe.br>
2017-06-19 21:26:34 -0300

    consulta funcionando com modelo OR

--- queryM.sql
+++ queryM.sql
@@ -6,9 +6,9 @@ ctx DBMS_XMLGEN.CTXHANDLE;
 XMLOUT CLOB;
 
 BEGIN
-      ctx := DBMS_XMLGEN.newContext('SELECT M.numero AS NUMERO, M.desc_terreno AS DESC_TERRENO, A.numero AS NUM_AVERBACAO FROM tb_matricula M, TABLE(M.averbacoes) A');
-      DBMS_XMLGEN.setRowsetTag(ctx, 'LISTA_MATRICULA');
-      DBMS_XMLGEN.setRowTag(ctx, 'MATRICULA');
+      ctx := DBMS_XMLGEN.newContext('SELECT A.CLIENTE.NOME AS CLIENTE_NOME, A.TIPO AS TIPO, A.IDADE AS IDADE FROM TB_ANIMAL A');
+      DBMS_XMLGEN.setRowsetTag(ctx, 'LISTA_ANIMAIS');
+      DBMS_XMLGEN.setRowTag(ctx, 'ANIMAL');
       XMLOUT := DBMS_XMLGEN.getXML(ctx);
       DBMS_OUTPUT.PUT_LINE(XMLOUT);
       DBMS_XMLGEN.closeContext(ctx);
